9a3ccc43976b781fedcdc5f6c7c9863bd5383cd3
Mohammad <gnuweb13@gmail.com>
2021-11-09 23:46:37 +0330

    add Reply post type

--- src/admin/post-type/post-type.php
+++ src/admin/post-type/post-type.php
@@ -16,9 +16,7 @@ class PostType {
 
     public static function tickets() {
         $labels = array(
-            'name'          => __( 'Tickets', 'helpdesk' ),
-            'singular_name' => __( 'Ticket', 'helpdesk' ),
-            'menu_name'     => __( 'Tickets', 'helpdesk' ),
+            'name' => __( 'Tickets', 'helpdesk' ),
         );
         $args = array(
             'labels'             => $labels,
@@ -26,15 +24,34 @@ class PostType {
             'publicly_queryable' => true,
             'show_ui'            => true,
             'show_in_menu'       => false,
-            'rewrite'            => array( 'slug' => 'ticket' ),
             'capability_type'    => 'post',
             'has_archive'        => true,
-            'supports'           => array( 'title', 'editor', 'author', 'thumbnail', 'comments' ),
+            'supports'           => array( 'title', 'editor', 'author', 'thumbnail' ),
             'show_in_rest'       => true
         );
 
         register_post_type( 'Ticket', $args );
     }
+
+    public static function reply() {
+        $labels = array(
+            'name' => __( 'Reply', 'helpdesk' ),
+        );
+        $args = array(
+            'labels'             => $labels,
+            'public'             => true,
+            'publicly_queryable' => true,
+            'show_ui'            => true,
+            'show_in_menu'       => false,
+            'capability_type'    => 'post',
+            'has_archive'        => true,
+            'supports'           => array( 'title', 'editor', 'author', 'thumbnail' ),
+            'show_in_rest'       => true
+        );
+
+        register_post_type( 'Reply', $args );
+    }
 }
 
 add_action( 'init', array( 'Helpdesk\Admin\PostType', 'tickets' ) );
+add_action( 'init', array( 'Helpdesk\Admin\PostType', 'reply' ) );
