457f6a0d56b5f93528041ec9b868c560a2715289
Mohammad <gnuweb13@gmail.com>
2021-12-26 20:47:34 +0330

    Prevent from deleting default statuses

--- src/agent-dashboard/app/src/components/Settings.js
+++ src/agent-dashboard/app/src/components/Settings.js
@@ -57,6 +57,13 @@ const Settings = () => {
     const [statusTerm, setStatus] = useState('');
     const [agentTerm, setAgent] = useState('');
 
+    const defaultStatus = [
+        'Open',
+        'Close',
+        'Pending',
+        'Resolved'
+    ]
+
     const {
         category,
         type,
@@ -343,9 +350,11 @@ const Settings = () => {
                                     <div key={status.id} className="helpdesk-term">
                                         <span>{status.name}</span>
                                         <div className="helpdesk-delete-term">
-                                            <Button onClick={() => deleteStatus(status.id, 'ticket_status')}>
-                                                <svg width="20" fill="#bfbdbd" viewBox="0 0 24 24"><path d="M19 6.41 17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z"></path></svg>
-                                            </Button>
+                                            { defaultStatus.indexOf(status.name) === -1 &&
+                                                <Button onClick={() => deleteStatus(status.id, 'ticket_status')}>
+                                                    <svg width="20" fill="#bfbdbd" viewBox="0 0 24 24"><path d="M19 6.41 17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z"></path></svg>
+                                                </Button>
+                                            }
                                         </div>
                                     </div>
                                 )
