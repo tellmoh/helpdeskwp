44468dc9e7ab498a45435e8381049f752868a386
Mohammad <gnuweb13@gmail.com>
2022-01-14 19:45:21 +0330

    add delete ticket to customers

--- src/agent-dashboard/app/src/routes/Customer.js
+++ src/agent-dashboard/app/src/routes/Customer.js
@@ -5,6 +5,11 @@ import { useParams, Link } from 'react-router-dom';
 import Pagination from '@mui/material/Pagination';
 import { ThemeProvider, createTheme } from '@mui/material/styles';
 import Stack from '@mui/material/Stack';
+import Button from '@mui/material/Button';
+import Swal from 'sweetalert2';
+import withReactContent from 'sweetalert2-react-content';
+
+const MySwal = withReactContent( Swal );
 
 const Customer = () => {
 	const [ customer, setCustomer ] = useState( null );
@@ -30,15 +35,15 @@ const Customer = () => {
 		},
 	} );
 
+	let config = {
+		headers: {
+			'X-WP-Nonce': helpdesk_agent_dashboard.nonce,
+			'Content-Type': 'application/json',
+		},
+	};
+
 	const fetchCustomer = async () => {
 		const url = `${ helpdesk_agent_dashboard.url }helpdesk/v1/settings/customer/${ params.id }`;
-
-		const config = {
-			headers: {
-				'X-WP-Nonce': helpdesk_agent_dashboard.nonce,
-				'Content-Type': 'application/json',
-			},
-		};
 
 		let data;
 		await axios.get( url, config ).then( ( res ) => {
@@ -75,6 +80,41 @@ const Customer = () => {
 		takeTickets( value );
 	};
 
+	const deleteTicket = async ( id ) => {
+		await axios
+			.delete(
+				`${ helpdesk_agent_dashboard.url }wp/v2/ticket/${ id }`,
+				config
+			)
+			.then( function ( res ) {
+				console.log( res.data.id );
+			} )
+			.catch( function ( err ) {
+				console.log( err );
+			} );
+
+		takeTickets();
+	};
+
+	const handleDelete = ( id ) => {
+		MySwal.fire( {
+			title: 'Are you sure?',
+			text: "You won't be able to revert this!",
+			icon: 'warning',
+			showCancelButton: true,
+			confirmButtonText: 'Delete',
+			cancelButtonText: 'Cancel',
+			reverseButtons: true,
+		} ).then( ( result ) => {
+			if ( result.isConfirmed ) {
+				deleteTicket( id );
+				MySwal.fire( 'Deleted', '', 'success' );
+			} else if ( result.dismiss === Swal.DismissReason.cancel ) {
+				MySwal.fire( 'Cancelled', '', 'error' );
+			}
+		} );
+	};
+
 	return (
 		<div className="helpdesk-main">
 			<div className="helpdesk-tickets">
@@ -92,13 +132,41 @@ const Customer = () => {
 									</h4>
 								</Link>
 								<div className="ticket-meta">
-									<div className="helpdesk-category">
-										{ __( 'In', 'helpdeskwp' ) }:{ ' ' }
-										{ ticket.category }
+									<div
+										className="helpdesk-w-50"
+										style={ { margin: 0 } }
+									>
+										<div className="helpdesk-category">
+											{ __( 'In', 'helpdeskwp' ) }:{ ' ' }
+											{ ticket.category }
+										</div>
+										<div className="helpdesk-type">
+											{ __( 'Type', 'helpdeskwp' ) }:{ ' ' }
+											{ ticket.type }
+										</div>
 									</div>
-									<div className="helpdesk-type">
-										{ __( 'Type', 'helpdeskwp' ) }:{ ' ' }
-										{ ticket.type }
+									<div
+										className="helpdesk-w-50"
+										style={ {
+											textAlign: 'right',
+											margin: 0,
+										} }
+									>
+										<Button
+											className="helpdesk-delete-ticket"
+											onClick={ ( e ) =>
+												handleDelete( ticket.id )
+											}
+										>
+											<svg
+												width="20"
+												fill="#0051af"
+												viewBox="0 0 24 24"
+												aria-hidden="true"
+											>
+												<path d="M14.12 10.47 12 12.59l-2.13-2.12-1.41 1.41L10.59 14l-2.12 2.12 1.41 1.41L12 15.41l2.12 2.12 1.41-1.41L13.41 14l2.12-2.12zM15.5 4l-1-1h-5l-1 1H5v2h14V4zM6 19c0 1.1.9 2 2 2h8c1.1 0 2-.9 2-2V7H6v12zM8 9h8v10H8V9z"></path>
+											</svg>
+										</Button>
 									</div>
 								</div>
 							</div>
