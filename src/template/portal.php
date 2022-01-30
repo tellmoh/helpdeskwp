6611d97229e6910868eb054ace20d8cfea2e938d
Mohammad <gnuweb13@gmail.com>
2022-01-30 22:00:57 +0330

    update portal template

--- src/template/portal.php
+++ src/template/portal.php
@@ -1,9 +1,15 @@
 <?php
 /**
- * Support portal page template
+ * Support portal page template.
+ *
+ * @package Help Desk WP
+ *
+ * @since 1.0.0
  *
  * @version 1.0.0
  */
+
+defined( 'ABSPATH' ) || exit;
 
 get_header();
 
