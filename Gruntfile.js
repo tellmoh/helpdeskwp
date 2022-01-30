1e592cb4be2a282d9168d1200366b7bfea323691
Mohammad <gnuweb13@gmail.com>
2022-01-30 21:54:36 +0330

    add watch and docs js

--- Gruntfile.js
+++ Gruntfile.js
@@ -7,20 +7,30 @@ module.exports = function (grunt) {
                 },
                 files: {
                     'src/assets/dist/css/login.css': 'src/assets/src/css/login.scss',
+                    'src/assets/dist/css/archive.css': 'src/assets/src/css/archive.scss',
+                    'src/assets/dist/css/single.css': 'src/assets/src/css/single.scss',
                 }
             }
         },
         uglify: {
             target: {
                 files: {
-                    'src/assets/dist/js/login.js': ['src/assets/src/js/login.js']
+                    'src/assets/dist/js/login.js': ['src/assets/src/js/login.js'],
+                    'src/assets/dist/js/docs.js': ['src/assets/src/js/docs.js']
                 }
+            }
+        },
+        watch: {
+            css: {
+                files: 'src/assets/src/css/*.scss',
+                tasks: ['sass'],
             }
         }
     });
 
     grunt.loadNpmTasks('grunt-contrib-sass');
     grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-watch');
 
     grunt.registerTask('default', ['sass', 'uglify']);
 };
--- readme.txt
+++ readme.txt
@@ -1,8 +1,8 @@
 === Help Desk WP ===
-Contributors: helpdeskwp, mhmdm
+Contributors: mhmdm
 Tags: customer support, support, support ticket, ticket system, helpdesk, tickets
 Requires at least: 5.2
-Tested up to: 5.8
+Tested up to: 5.9
 Stable tag: 1.1.0
 License: GPL v2 or later
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
