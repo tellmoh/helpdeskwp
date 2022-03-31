97aef14c38ef55401b5891ade9f9765f3bf5cc3f
Mohammad <gnuweb13@gmail.com>
2022-04-01 00:10:14 +0430

    add grunt compress

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,34 +3,65 @@ module.exports = function (grunt) {
         sass: {
             dist: {
                 options: {
-                    style: 'expanded'
+                    style: "expanded",
                 },
                 files: {
-                    'src/assets/dist/css/login.css': 'src/assets/src/css/login.scss',
-                    'src/assets/dist/css/archive.css': 'src/assets/src/css/archive.scss',
-                    'src/assets/dist/css/single.css': 'src/assets/src/css/single.scss',
-                }
-            }
+                    "src/assets/dist/css/login.css":
+                        "src/assets/src/css/login.scss",
+                    "src/assets/dist/css/archive.css":
+                        "src/assets/src/css/archive.scss",
+                    "src/assets/dist/css/single.css":
+                        "src/assets/src/css/single.scss",
+                },
+            },
         },
         uglify: {
             target: {
                 files: {
-                    'src/assets/dist/js/login.js': ['src/assets/src/js/login.js'],
-                    'src/assets/dist/js/docs.js': ['src/assets/src/js/docs.js']
-                }
-            }
+                    "src/assets/dist/js/login.js": [
+                        "src/assets/src/js/login.js",
+                    ],
+                    "src/assets/dist/js/docs.js": ["src/assets/src/js/docs.js"],
+                },
+            },
+        },
+        compress: {
+            main: {
+                options: {
+                    archive: "../helpdeskwp.zip",
+                },
+                files: [
+                    {
+                        src: [
+                            "src/**",
+                            "changelog.txt",
+                            "helpdeskwp.php",
+                            "readme.txt",
+                        ],
+                        filter: function (filepath) {
+                            return (
+                                grunt.file.isFile(filepath) &&
+                                filepath.indexOf("node_modules") < 0
+                            );
+                        },
+                        dest: "/helpdeskwp",
+                    },
+                ],
+            },
         },
         watch: {
             css: {
-                files: 'src/assets/src/css/*.scss',
-                tasks: ['sass'],
-            }
-        }
+                files: "src/assets/src/css/*.scss",
+                tasks: ["sass"],
+            },
+        },
     });
 
-    grunt.loadNpmTasks('grunt-contrib-sass');
-    grunt.loadNpmTasks('grunt-contrib-uglify');
-    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks("grunt-contrib-sass");
+    grunt.loadNpmTasks("grunt-contrib-uglify");
+    grunt.loadNpmTasks("grunt-contrib-watch");
+    grunt.loadNpmTasks("grunt-contrib-compress");
 
-    grunt.registerTask('default', ['sass', 'uglify']);
+    grunt.registerTask("default", ["sass", "uglify"]);
+    grunt.registerTask("package", ["sass", "uglify", "compress"]);
 };
