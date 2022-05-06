9ae0c4752ef6b7785f73bd1515be4f7bbaa9acff
Mohammad <gnuweb13@gmail.com>
2022-05-06 20:27:33 +0430

    remove / from path

--- helpdeskwp.php
+++ helpdeskwp.php
@@ -14,9 +14,9 @@ defined( 'ABSPATH' ) || exit;
 
 define( 'HELPDESKWP', '1.2.0' );
 
-define( 'HELPDESK_WP_PATH', plugin_dir_path( __FILE__ ) . '/' );
+define( 'HELPDESK_WP_PATH', plugin_dir_path( __FILE__ ) );
 
-define( 'HELPDESK_WP_URL', plugin_dir_url( __FILE__ ) . '/' );
+define( 'HELPDESK_WP_URL', plugin_dir_url( __FILE__ ) );
 
 require HELPDESK_WP_PATH . 'src/helpdeskwp.php';
 
